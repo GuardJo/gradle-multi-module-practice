d4b81895ba1eca1e9d6fd4fad636a52d934cd8b9
kyeongho <dagh1218@naver.com>
2023-09-16 00:34:24 +0900

    #1 bean 주입 방식 변경
    - SpringBootApplication에서 주입을 추가하는 것이 아닌 별도 Config 객체에서 추가 componentScan 지정

--- multi-module-api/src/main/java/io/github/guardjo/multimodule/api/MultiModuleApiApplication.java
+++ multi-module-api/src/main/java/io/github/guardjo/multimodule/api/MultiModuleApiApplication.java
@@ -3,10 +3,7 @@ package io.github.guardjo.multimodule.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-        "io.github.guardjo.multimodule.common",
-        "io.github.guardjo.multimodule.api"
-})
+@SpringBootApplication
 public class MultiModuleApiApplication {
 
     public static void main(String[] args) {
