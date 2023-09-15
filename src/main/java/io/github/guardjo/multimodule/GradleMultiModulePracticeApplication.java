0d5c633dac92386a705a29d4da6874899ca0dabc
kyeongho <dagh1218@naver.com>
2023-09-15 23:55:48 +0900

    #1 gradle 프로젝트 생성
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/io/github/guardjo/multimodule/GradleMultiModulePracticeApplication.java
@@ -0,0 +1,13 @@
+package io.github.guardjo.multimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleMultiModulePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleMultiModulePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/guardjo/multimodule/GradleMultiModulePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guardjo.multimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleMultiModulePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
