c3a76d765ddc1c68c6778caf1b4bfc58d4907373
kyeongho <dagh1218@naver.com>
2023-09-21 23:32:59 +0900

    #5 common 모듈내 DB 관련 의존성 및 객체 추가
    - orm을 위한 spring-data-jpa 의존성 추가
    -- 상위 모듈에서도 사용할 수 있게끔 api 로 의존성 주입
    - h2 DB 사용을 위한 h2 의존성 추가
    - 테스트를 위한 Entity, Repository 및 service 메소드 구현

---
+++ multi-module-common/src/main/java/io/github/guardjo/multimodule/common/domain/Member.java
@@ -0,0 +1,20 @@
+package io.github.guardjo.multimodule.common.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
--- multi-module-common/src/main/java/io/github/guardjo/multimodule/common/service/TestService.java
+++ multi-module-common/src/main/java/io/github/guardjo/multimodule/common/service/TestService.java
@@ -1,12 +1,20 @@
 package io.github.guardjo.multimodule.common.service;
 
+import io.github.guardjo.multimodule.common.domain.Member;
 import io.github.guardjo.multimodule.common.exception.TestException;
 import io.github.guardjo.multimodule.common.model.TestModel;
+import io.github.guardjo.multimodule.common.repository.MemberRepository;
 import io.github.guardjo.multimodule.common.type.ResponseCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class TestService {
+    private final MemberRepository memberRepository;
+
     public TestModel findTestModel() {
         return TestModel.builder()
                 .name("tester")
@@ -15,5 +23,13 @@ public class TestService {
 
     public void throwException() throws TestException {
         throw new TestException(ResponseCode.FAIL);
+    }
+
+    public void saveMember(Member member) {
+        memberRepository.save(member);
+    }
+
+    public List<Member> findAllMember() {
+        return memberRepository.findAll();
     }
 }
