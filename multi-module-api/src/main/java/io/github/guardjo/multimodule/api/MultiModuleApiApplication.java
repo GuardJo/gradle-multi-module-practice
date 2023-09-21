e9fe777994afd4c5fc526262314a244633e8587e
kyeongho <dagh1218@naver.com>
2023-09-21 23:34:13 +0900

    #5 api 모듈 내 DB 관련 설정 추가
    - jpa 관련 객체 bean 주입을 위한 scan 설정
    - DB 연결 설정 추가
    - Entity 저장 및 조회 api 구현

--- multi-module-api/src/main/java/io/github/guardjo/multimodule/api/MultiModuleApiApplication.java
+++ multi-module-api/src/main/java/io/github/guardjo/multimodule/api/MultiModuleApiApplication.java
@@ -2,8 +2,12 @@ package io.github.guardjo.multimodule.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "io.github.guardjo.multimodule.common.repository")
+@EntityScan(basePackages = "io.github.guardjo.multimodule.common.domain")
 public class MultiModuleApiApplication {
 
     public static void main(String[] args) {
--- multi-module-api/src/main/java/io/github/guardjo/multimodule/api/controller/TestController.java
+++ multi-module-api/src/main/java/io/github/guardjo/multimodule/api/controller/TestController.java
@@ -1,6 +1,7 @@
 package io.github.guardjo.multimodule.api.controller;
 
 import io.github.guardjo.multimodule.api.response.CommonResponse;
+import io.github.guardjo.multimodule.common.domain.Member;
 import io.github.guardjo.multimodule.common.exception.TestException;
 import io.github.guardjo.multimodule.common.model.TestModel;
 import io.github.guardjo.multimodule.common.service.TestService;
@@ -8,7 +9,11 @@ import io.github.guardjo.multimodule.common.type.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -44,5 +49,35 @@ public class TestController {
     public void exception() throws TestException {
         log.info("Request Exception");
         testService.throwException();
+    }
+
+    @PostMapping("/member")
+    public CommonResponse<String> saveMember(@RequestParam("name") String name) {
+        log.info("Request Save New Member");
+
+        Member member = Member.builder()
+                .name(name)
+                .build();
+
+        testService.saveMember(member);
+
+        return CommonResponse.<String>builder()
+                .responseCode(ResponseCode.SUCCESS.getCode())
+                .responseMessage(ResponseCode.SUCCESS.getMessage())
+                .info("Save Successes")
+                .build();
+    }
+
+    @GetMapping("/members")
+    public CommonResponse<List<Member>> findAllMember() {
+        log.info("Request FindAll Member");
+
+        List<Member> members = testService.findAllMember();
+
+        return CommonResponse.<List<Member>>builder()
+                .responseCode(ResponseCode.SUCCESS.getCode())
+                .responseMessage(ResponseCode.SUCCESS.getMessage())
+                .info(members)
+                .build();
     }
 }
