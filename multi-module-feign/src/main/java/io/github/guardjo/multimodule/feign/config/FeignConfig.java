d32264a594b4b938da31a7543d365c0d0cbe190c
kyeongho <dagh1218@naver.com>
2023-09-24 20:10:47 +0900

    #11 feign 의 Interceptor 구성
    - RequedstInterceptor에 대한 구현체 구현
    -- option에 대한 기본값을 요청 보내기 전에 주입하도록 구현함

--- multi-module-feign/src/main/java/io/github/guardjo/multimodule/feign/config/FeignConfig.java
+++ multi-module-feign/src/main/java/io/github/guardjo/multimodule/feign/config/FeignConfig.java
@@ -1,7 +1,13 @@
 package io.github.guardjo.multimodule.feign.config;
 
+import io.github.guardjo.multimodule.feign.interceptor.DemoInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+    @Bean
+    public DemoInterceptor demoInterceptor() {
+        return DemoInterceptor.create();
+    }
 }
