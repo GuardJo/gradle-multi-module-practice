66f8db06193573223ca6c00931936de13555b53c
kyeongho <dagh1218@naver.com>
2023-09-24 19:30:43 +0900

    #11 api 모듈의 external api 호출용 client 구성
    - 외부 api 호출용 feign client 신규 구성
    -- api 모듈의 external을 기본 prefix로 설정
    -- default 설정과 demo-client용 설정 추가
    -- 응답 객체 구성 및 feign client 활성화 지정

---
+++ multi-module-feign/src/main/java/io/github/guardjo/multimodule/feign/client/DemoClient.java
@@ -0,0 +1,20 @@
+package io.github.guardjo.multimodule.feign.client;
+
+import io.github.guardjo.multimodule.feign.config.FeignConfig;
+import io.github.guardjo.multimodule.feign.data.ResponseInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "demo-client",
+        url = "${feign.url.prefix}",
+        configuration = FeignConfig.class
+)
+public interface DemoClient {
+    @GetMapping
+    ResponseEntity<ResponseInfo> callGet(@RequestHeader("external-header") String header,
+                                         @RequestParam("name") String name);
+}
---
+++ multi-module-feign/src/main/java/io/github/guardjo/multimodule/feign/config/FeignConfig.java
@@ -0,0 +1,7 @@
+package io.github.guardjo.multimodule.feign.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+}
