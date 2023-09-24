8176a854c5dbd3c0dbce04ebc1af676f962f47d5
kyeongho <dagh1218@naver.com>
2023-09-24 16:58:16 +0900

    #9 비동기처리 작업용 모듈 생성
    - spring boot 2.7.16
    -- spring-boot-web, lombok 의존성 추가
    - root setting에 관련 모듈 추가

---
+++ multi-moudule-async/src/main/java/io/github/guardjo/multimoudule/async/MultiMouduleAsyncApplication.java
@@ -0,0 +1,13 @@
+package io.github.guardjo.multimoudule.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiMouduleAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiMouduleAsyncApplication.class, args);
+    }
+
+}
