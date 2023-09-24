3d1f1ac7b1a910445cdc388ab7b8b8aea7c44ee4
kyeongho <dagh1218@naver.com>
2023-09-24 18:56:06 +0900

    #11 feign 클라이언트용 모듈 구성
    - feign 클라이언트 적용 모듈 생성
    -- spring-boot-web, lombok, openfeign 의존성 추가

---
+++ multi-module-feign/src/main/java/io/github/guardjo/multimodule/feign/MultiModuleFeignApplication.java
@@ -0,0 +1,13 @@
+package io.github.guardjo.multimodule.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleFeignApplication.class, args);
+    }
+
+}
