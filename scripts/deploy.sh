30e075435900750cc74fb57c049ec82f57c17409
kyeongho <dagh1218@naver.com>
2023-10-02 21:00:19 +0900

    #15 배포스크립트 출력 log 추가
    - stdout일 경우 백그라운드 배포하더라도 ApplicationStart task가 올바르게 끝나지 않기에 별도 log로 분리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 API_JAR_PATH="/home/ubuntu/code-deploy/gradle-multi-module-practice/multi-module-api/build/libs/*.jar"
+DEPLOY_LOG="/home/ubuntu/code-deploy/deploy.log"
+DEPLOY_ERR_LOG="/home/ubuntu/code-deploy/deploy-error.log"
 BUILD_JAR=$(ls $API_JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
@@ -15,6 +17,6 @@ fi
 
 DEPLOY_JAR="$DEPLOY_PATH/$JAR_NAME"
 
-nohup java -jar -Dspring.profiles.active=test $API_JAR_PATH &
+nohup java -jar -Dspring.profiles.active=test $API_JAR_PATH >> $DEPLOY_LOG 2> $DEPLOY_ERR_LOG &
 
 sleep 3
